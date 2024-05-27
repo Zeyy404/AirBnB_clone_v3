bd0158f94f6bee1cd02e43a6af06b26cbe5bf5ed
Zainab Salih <Zainab.Salih@outlook.com>
2024-05-27 16:48:36 +0000

    Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,10 +12,12 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """Closes the storage on teardown"""
     storage.close()
+
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,7 @@
 from flask import jsonify
 from . import app_views
 
+
 @app_views.route('/status', methods=['GET'])
 def status():
     return jsonify({"status": "OK"})
