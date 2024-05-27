1a31b065aa0384b1e36b1b4071c28ea684c7b7a4
Zainab Salih <Zainab.Salih@outlook.com>
2024-05-27 17:14:02 +0000

    Some stats?

--- api/v1/app.py
+++ api/v1/app.py
@@ -19,6 +19,12 @@ def teardown_db(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(error):
+    """Handler for 404 errors"""
+    return jsonify({"error": "Not found"}), 404
+
+
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', 5000))
